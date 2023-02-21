16ce95e5c72f9f89b0ddd9a8e0843c4c1b95c56b
sample <youngjae7981@gmail.com>
2023-02-21 16:47:02 +0900

    가야지 가야지

--- newLogin.js
+++ newLogin.js
@@ -1,40 +1,20 @@
-// const form = document.querySelector("#login-form");
-// const input = document.querySelector('#login-form input[type="text"]');
-
-// form.addEventListener("submit", function (L) {
-//   L.preventDefault();
-//   const inputValue = input.value;
-//   localStorage.setItem("name", inputValue);
-//   localStorage.setItem("name", inputValue);
-//   const greeting = document.querySelector("#greeting");
-//   greeting.textContent = `안녕 반가워, ${inputValue}!`;
-//   greeting.classList.remove("hidden");
-// });
-const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 function onSubmit(e) {
-  if (loginInput.value.trim() === "") {
-    alert("Please enter your name.");
-    e.preventDefault();
-  } else {
-    localStorage.setItem("username", loginInput.value);
-    displayGreeting();
-  }
+  e.preventDefault();
+  localStorage.setItem("username", loginInput.value);
+  greeting.innerHTML = "Hello, " + loginInput.value;
+  greeting.classList.remove("hidden");
+  loginForm.classList.add("hidden");
 }
 
-function displayGreeting() {
-  const storedName = localStorage.getItem("username");
-  if (storedName) {
-    greeting.textContent = `안녕, ${storedName}!`;
-    greeting.classList.remove("hidden");
-  }
-}
-
-displayGreeting();
 loginForm.addEventListener("submit", onSubmit);
 
-// 1) 로컬스토리지를 읽어서 이름이 있는지 확인
-// 2) 이름이 있으면 H1태그에 "HELLO,이름"출력
-// 3) HIDDEN 클래스 삭제
+const savedUserName = localStorage.getItem("username");
+if (savedUserName != null) {
+  greeting.innerHTML = "Hello, " + savedUserName;
+  greeting.classList.remove("hidden");
+  loginForm.classList.add("hidden");
+}
